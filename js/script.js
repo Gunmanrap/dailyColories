443274c67fa94aa2c5250d373393638e39f7a9c0
Gunmanrap <gunmanrap@gmail.com>
2024-04-08 14:23:59 +0300

    daily colorie project

--- js/script.js
+++ js/script.js
@@ -1,67 +1,74 @@
-personDB = {
-    gender: null,
-    age: null,
-    weight: null,
-    height: null,
-    activity: {'мінімальны': 1.2, 'нізкі': 1.375, 'умераны': 1.55, 'высокі': 1.725, 'экстрэмальны': 1.9},
-    activityCoefficient: null,
-    dailyCalorie: null,
-    indexBMI: {
-        'Ніжэй за нармальную вагу': [18.5],
-        'Нармальная вага': [18.5, 25],
-        'Залішняя вага': [25, 30],
-        'Атлусценне 1 ступені': [30, 35],
-        'Атлусценне 2 ступені': [35, 40]
-    },
-    start() {
-        this.gender = prompt('Увядзіце ваш пол: м - мужчына, ж - жанчына');
-        if (this.gender !== 'м' && this.gender !== 'ж') {
-            alert('Вы уводзіце некарэктныя дадзеныя!');
-            this.gender = prompt('Увядзіце ваш пол: м - мужчына, ж - жанчына');
-        }
+let form = document.forms['main'],
+    steps = document.querySelectorAll('.step'),
+    gender, activity, age, growth, weight, dailyCalorie;
+document.querySelector('.calculator-gender__items').addEventListener('click', genderFunc);
 
-        this.age = +prompt('Увядзіце ваш узрост');
-        if (checkValue(this.age)) {
-            this.age = +prompt('Увядзіце ваш узрост');
-        }
-
-        this.weight = +prompt('Увядзіце вашу вагу');
-
-        this.height = +prompt('Увядзіце ваш рост');
-
-        this.activityCoefficient = this.activity[prompt('Увядзіце ўзровень вашай актыўнасці: мінімальны,' +
-            ' нізкі, умераны, высокі, экстрэмальны')];
-        if (this.activityCoefficient === undefined) {
-            alert('Вы уводзіце некарэктныя дадзеныя!')
-            this.activityCoefficient = this.activity[prompt('Увядзіце ўзровень вашай актыўнасці: мінімальны,' +
-                ' нізкі, умераны, высокі, экстрэмальны')];
-        }
-    },
-    getBMI() {
-        if (!this.weight && !this.height) {
-            alert('Запусціце метад personDB.start');
-        }
-        const index = +(Math.pow((this.weight / this.height), 2) * 100);
-    },
-    getDailyCalorie() {
-        if (!this.age && !this.weight && !this.height && !this.gender && !this.activityCoefficient) {
-            alert('Запусціце метад personDB.start');
-        } else if (this.gender === 'м') {
-            this.dailyCalorie = ((10 * this.weight) + (6.25 * this.height) - (5 * this.age) + 5) * this.activityCoefficient;
-        } else if (this.gender === 'ж') {
-            this.dailyCalorie = ((10 * this.weight) + (6.25 * this.height) - (5 * this.age) - 161) * this.activityCoefficient;
-        }
+function genderFunc(event) {
+    document.querySelectorAll('.calculator-gender__item').forEach(el => {
+        el.classList.remove('action');
+    })
+    if (event.target.parentElement.closest('.calculator-gender__item')) {
+        event.target.parentElement.closest('.calculator-gender__item').classList.add('action');
     }
 }
 
-function checkValue(value) {
-    if (value === '' || value <= 0 || isNaN(value)) {
-        alert('Вы уводзіце некарэктныя дадзеныя!');
-        return true;
+document.querySelector('#step-1-btn').addEventListener('click', (event) => {
+    event.preventDefault();
+    steps[0].style.display = 'none';
+    steps[1].style.display = 'inherit';
+    gender = form.gender.value;
+});
+
+
+document.querySelector('.calculator-activity__items').addEventListener('click', activityFunc);
+
+function activityFunc(event) {
+    document.querySelectorAll('.calculator-activity__item').forEach(el => {
+        el.classList.remove('action');
+    })
+    if (event.target.parentElement.closest('.calculator-activity__item')) {
+        event.target.parentElement.closest('.calculator-activity__item').classList.add('action');
     }
 }
 
-personDB.start()
-personDB.getBMI()
-personDB.getDailyCalorie()
-console.log(personDB)
+document.querySelector('#step-2-btn').addEventListener('click', (event) => {
+    event.preventDefault();
+    steps[1].style.display = 'none';
+    steps[2].style.display = 'inherit';
+    activity = form.activity.value;
+});
+
+document.querySelectorAll('.data__block-item input').forEach(el => {
+    el.addEventListener('blur', checkData);
+})
+
+function checkData(event) {
+    if (!event.target.validity.rangeOverflow && !event.target.validity.rangeUnderflow) {
+        if (event.target.name === 'age' && Boolean(event.target.value)) {
+            age = event.target.value;
+        } else if (event.target.name === 'growth' && Boolean(event.target.value)) {
+            growth = event.target.value;
+        } else if (event.target.name === 'weight' && Boolean(event.target.value)) {
+            weight = event.target.value;
+        }
+    } else {
+        event.target.value = '';
+    }
+}
+
+document.querySelector('#step-result').addEventListener('click', (event) => {
+    event.preventDefault();
+    steps[2].style.display = 'none';
+    document.querySelector('.calculator-result').style.display = 'inherit';
+    const index = (Math.pow((weight / growth), 2) * 100).toFixed(1);
+    document.querySelector('#imt-value').textContent = String(index);
+    document.querySelector('#nc').textContent = String(getDailyCalorie());
+});
+
+function getDailyCalorie() {
+    if (gender === 'man') {
+        return dailyCalorie = Math.floor((((10 * weight) + (6.25 * growth) - (5 * age) + 5) * activity));
+    } else {
+        return dailyCalorie = Math.floor((((10 * weight) + (6.25 * growth) - (5 * age) - 161) * activity));
+    }
+}
